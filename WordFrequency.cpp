1c862e4a20f3c8ed0c870fdb8538c0d6414781e8
Adrian Javorski <ajavorski784@csu.fullerton.edu>
2019-12-20 20:00:26 -0800

    Finished with commenting for code readability, and also formatted code. Passed all unit tests, and made sure program compiles.

--- WordFrequency.cpp
+++ WordFrequency.cpp
@@ -1,42 +1,24 @@
 #include "WordFrequency.h"
 #include "sanitize.h"
-#include <algorithm>
 #include <fstream>
-#include <sstream>
 #include <string>
 #include <unordered_map>
 
 WordFrequency::WordFrequency() {} // default constructor
 void WordFrequency::readIn(const string &filename) {
   std::ifstream file(filename);
-  std::string line; // variable to allow for reading a whole line in to process
   std::string currentWord; // Word holder for placing into hash table
 
-  while (!file.eof()) {
-    getline(file, line); // Reads line in from text
-    std::stringstream currentLine(line);
-    while (!currentLine.eof()) {
-      getline(currentLine, currentWord, ' '); // Reads a word from the stream
-      // Removal of end punctuation characters
-      currentWord.erase(
-          std::remove(currentWord.begin(), currentWord.end(), '.'),
-          currentWord.end());
-      currentWord.erase(
-          std::remove(currentWord.begin(), currentWord.end(), '?'),
-          currentWord.end());
-      currentWord.erase(
-          std::remove(currentWord.begin(), currentWord.end(), '!'),
-          currentWord.end());
-
-      sanitize(currentWord); // Sanitizes un needed characters in string
-
-      if (currentWord == "") { // Just checks if any string is an empty string
-        continue;              // Don't put empty strings into Frequency table
-      } else {
-        frequencyTable[currentWord]++; // Add word to Frequency table
-      }
+  while (file >> currentWord) {
+    sanitize(currentWord);
+    char e = currentWord[currentWord.length() - 1]; // stores last character
+    if (e == '.' || e == '!' || e == '?') {
+      // Checks if last character is end punctuation
+      currentWord.pop_back(); // removes end punctuation if exists
     }
+    frequencyTable[currentWord]++; // Adds word to hash table
   }
+
 } // add words from file to hash table
 size_t WordFrequency::numberOfWords() {
   return frequencyTable.size();
@@ -46,8 +28,8 @@ size_t WordFrequency::wordCount(const string &word) {
 
 } // return the number of occurrences of the given word
 string WordFrequency::mostFrequentWord() {
-  std::string word;
-  int freqWord = 0;
+  std::string word; // The current highest frequency word
+  int freqWord = 0; // Holds the highest frequency to compare to others
   std::unordered_map<string, int>::iterator itr;
   for (itr = frequencyTable.begin(); itr != frequencyTable.end(); itr++) {
     if (itr == frequencyTable.begin()) {
